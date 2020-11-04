693ddf2812acd81ff43b01d5995d544abffb1a99
jingyuan-git <lujingyuan95@gmail.com>
2020-11-04 14:22:46 +0800

    26 remove duplicates from sorted array

---
+++ leetcode/editor/cn/[25]Reverse Nodes in k-Group.go
@@ -0,0 +1,69 @@
+//Given a linked list, reverse the nodes of a linked list k at a time and return
+// its modified list. 
+//
+// k is a positive integer and is less than or equal to the length of the linked
+// list. If the number of nodes is not a multiple of k then left-out nodes, in the
+// end, should remain as it is. 
+//
+// Follow up: 
+//
+// 
+// Could you solve the problem in O(1) extra memory space? 
+// You may not alter the values in the list's nodes, only nodes itself may be ch
+//anged. 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,2,3,4,5], k = 2
+//Output: [2,1,4,3,5]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,2,3,4,5], k = 3
+//Output: [3,2,1,4,5]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: head = [1,2,3,4,5], k = 1
+//Output: [1,2,3,4,5]
+// 
+//
+// Example 4: 
+//
+// 
+//Input: head = [1], k = 1
+//Output: [1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range sz. 
+// 1 <= sz <= 5000 
+// 0 <= Node.val <= 1000 
+// 1 <= k <= sz 
+// 
+// Related Topics 链表 
+// 👍 781 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseKGroup(head *ListNode, k int) *ListNode {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/cn/[26]Remove Duplicates from Sorted Array.go
@@ -0,0 +1,77 @@
+//Given a sorted array nums, remove the duplicates in-place such that each eleme
+//nt appears only once and returns the new length. 
+//
+// Do not allocate extra space for another array, you must do this by modifying 
+//the input array in-place with O(1) extra memory. 
+//
+// Clarification: 
+//
+// Confused why the returned value is an integer but your answer is an array? 
+//
+// Note that the input array is passed in by reference, which means a modificati
+//on to the input array will be known to the caller as well. 
+//
+// Internally you can think of this: 
+//
+// 
+//// nums is passed in by reference. (i.e., without making a copy)
+//int len = removeDuplicates(nums);
+//
+//// any modification to nums in your function would be known by the caller.
+//// using the length returned by your function, it prints the first len element
+//s.
+//for (int i = 0; i < len; i++) {
+//    print(nums[i]);
+//} 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,1,2]
+//Output: 2, nums = [1,2]
+//Explanation: Your function should return length = 2, with the first two elemen
+//ts of nums being 1 and 2 respectively. It doesn't matter what you leave beyond t
+//he returned length.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [0,0,1,1,1,2,2,3,3,4]
+//Output: 5, nums = [0,1,2,3,4]
+//Explanation: Your function should return length = 5, with the first five eleme
+//nts of nums being modified to 0, 1, 2, 3, and 4 respectively. It doesn't matter 
+//what values are set beyond the returned length.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 0 <= nums.length <= 3 * 104 
+// -104 <= nums[i] <= 104 
+// nums is sorted in ascending order. 
+// 
+// Related Topics 数组 双指针 
+// 👍 1701 👎 0
+
+package main
+
+// 双指针
+//leetcode submit region begin(Prohibit modification and deletion)
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 1{
+		return len(nums)
+	}
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	// 由于下标从0开始，所以nums的长度和下标相差1
+	return i + 1
+}
+//leetcode submit region end(Prohibit modification and deletion)
