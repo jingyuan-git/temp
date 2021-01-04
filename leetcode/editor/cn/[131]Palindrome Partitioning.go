c0d110db16f09488b70bf3e8e8a6a543a80f3d40
jingyuan-git <lujingyuan95@gmail.com>
2021-01-04 13:57:57 +0800

    131 palindrome partitioning

---
+++ leetcode/editor/cn/[131]Palindrome Partitioning.go
@@ -0,0 +1,29 @@
+//Given a string s, partition s such that every substring of the partition is a 
+//palindrome. Return all possible palindrome partitioning of s. 
+//
+// A palindrome string is a string that reads the same backward as forward. 
+//
+// 
+// Example 1: 
+// Input: s = "aab"
+//Output: [["a","a","b"],["aa","b"]]
+// Example 2: 
+// Input: s = "a"
+//Output: [["a"]]
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 16 
+// s contains only lowercase English letters. 
+// 
+// Related Topics 深度优先搜索 动态规划 回溯算法 
+// 👍 451 👎 0
+package main
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func partition(s string) [][]string {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
