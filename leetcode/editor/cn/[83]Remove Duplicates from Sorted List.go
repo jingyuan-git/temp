dc028d1dc82fd1d14daf6f66ac41ae16c28a93ea
jingyuan-git <lujingyuan95@gmail.com>
2021-01-15 18:30:17 +0800

    83 remove duplicates from sorted list

---
+++ leetcode/editor/cn/[83]Remove Duplicates from Sorted List.go
@@ -0,0 +1,70 @@
+//Given the head of a sorted linked list, delete all duplicates such that each e
+//lement appears only once. Return the linked list sorted as well. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,1,2]
+//Output: [1,2]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,1,2,3,3]
+//Output: [1,2,3]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range [0, 300]. 
+// -100 <= Node.val <= 100 
+// The list is guaranteed to be sorted in ascending order. 
+// 
+// Related Topics 链表 
+// 👍 451 👎 0
+
+package main
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	current := head
+	for current != nil && current.Next != nil {
+		if current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+			continue
+		}
+		current = current.Next
+	}
+	return head
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+/*
+public ListNode deleteDuplicates(ListNode head) {
+    ListNode current = head;
+    while (current != null && current.next != null) {
+        if (current.next.val == current.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return head;
+}
+复杂度分析
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/solution/shan-chu-pai-xu-lian-biao-zhong-de-zhong-fu-yuan-s/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
