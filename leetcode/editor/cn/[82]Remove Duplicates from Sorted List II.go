f35f11ccaf135f316d6bc6fedd13eb7edd6360ef
jingyuan-git <lujingyuan95@gmail.com>
2021-01-15 13:44:40 +0800

    82 remove duplicates from sorted list II (unfinished)

---
+++ leetcode/editor/cn/[82]Remove Duplicates from Sorted List II.go
@@ -0,0 +1,55 @@
+//Given the head of a sorted linked list, delete all nodes that have duplicate n
+//umbers, leaving only distinct numbers from the original list. Return the linked 
+//list sorted as well. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,2,3,3,4,4,5]
+//Output: [1,2,5]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,1,1,2,3]
+//Output: [2,3]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range [0, 300]. 
+// -100 <= Node.val <= 100 
+// The list is guaranteed to be sorted in ascending order. 
+// 
+// Related Topics 链表 
+// 👍 429 👎 0
+
+package main
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	pre := &ListNode{Val: 0, Next: head}
+	root := pre
+	temp := head
+
+	for temp.Next != nil {
+		if temp.Val == temp.Next.Val {
+			temp = temp.Next
+			continue
+		}
+		pre.Next = temp.Next
+		temp = temp.Next
+	}
+	return root.Next
+}
+//leetcode submit region end(Prohibit modification and deletion)
