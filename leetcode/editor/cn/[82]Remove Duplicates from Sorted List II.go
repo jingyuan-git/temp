d63ac19166411b4c1e946780440c8b5f9b7e28ea
jingyuan-git <lujingyuan95@gmail.com>
2021-01-18 17:51:13 +0800

    82 remove duplicates from sorted list II

--- leetcode/editor/cn/[82]Remove Duplicates from Sorted List II.go
+++ leetcode/editor/cn/[82]Remove Duplicates from Sorted List II.go
@@ -37,19 +37,53 @@ package main
  *     Next *ListNode
  * }
  */
+// TODO: 我之前考虑的是pre，current，next。比较好的写法：把head指针提前一位，然后判断head.next与head.next.next
 func deleteDuplicates(head *ListNode) *ListNode {
-	pre := &ListNode{Val: 0, Next: head}
-	root := pre
-	temp := head
-
-	for temp.Next != nil {
-		if temp.Val == temp.Next.Val {
-			temp = temp.Next
-			continue
+	node := &ListNode{Val: 0, Next: head}
+	current, first := node, node
+	for current.Next != nil && current.Next.Next != nil {
+		if current.Next.Val == current.Next.Next.Val {
+			first = current.Next
+			for current.Next != nil && current.Next.Val == first.Val {
+				current.Next = current.Next.Next
+			}
+		} else {
+			current = current.Next
 		}
-		pre.Next = temp.Next
-		temp = temp.Next
 	}
-	return root.Next
+
+	return node.Next
+
+	//for temp.Next != nil {
+	//	if temp.Val == temp.Next.Val {
+	//		temp = temp.Next
+	//		continue
+	//	}
+	//	pre.Next = temp.Next
+	//	temp = temp.Next
+	//}
+	//return node.Next
 }
 //leetcode submit region end(Prohibit modification and deletion)
+/*
+func deleteDuplicates(head *ListNode) *ListNode {
+    node := &ListNode{0, head}
+	head, first := node, node
+	for head.Next != nil && head.Next.Next != nil {
+		if head.Next.Val == head.Next.Next.Val {
+			first.Val = head.Next.Val
+			for head.Next != nil && first.Val == head.Next.Val {
+				head.Next = head.Next.Next
+			}
+		} else {
+			head = head.Next
+		}
+	}
+	return node.Next
+}
+
+作者：user1775a
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/solution/golang-yi-ci-xun-huan-by-user1775a/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
