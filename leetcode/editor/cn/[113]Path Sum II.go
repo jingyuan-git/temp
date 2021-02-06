38bd84a5d2535f89491493006a9123d5339345ce
jingyuan <lujingyuan95@gmail.com>
2021-02-06 22:00:06 +0800

    113 path sum II finished

--- leetcode/editor/cn/[113]Path Sum II.go
+++ leetcode/editor/cn/[113]Path Sum II.go
@@ -49,22 +49,55 @@ package main
 
 
 func pathSum(root *TreeNode, targetSum int) (res [][]int) {
-	var dfs func(path []int, root *TreeNode, targetSum int)
-	dfs = func(path []int, root *TreeNode, targetSum int) {
+	path := []int{}
+	var dfs func(*TreeNode, int)
+	dfs = func(root *TreeNode, targetSum int) {
 		if root == nil {
 			return
 		}
 
+		// TODO: 注意defer函数的顺序
+		path = append(path, root.Val)
 		defer func() { path = path[:len(path)-1] }()
-		if root.Left == nil && root.Right == nil && targetSum == 0 {
+
+		if root.Left == nil && root.Right == nil && targetSum - root.Val == 0 {
 			res = append(res, append([]int(nil), path...))
 			return
 		}
-		dfs(append(path, root.Val), root.Left, targetSum - root.Val)
-		dfs(append(path, root.Val), root.Right, targetSum - root.Val)
+
+		dfs(root.Left, targetSum - root.Val)
+		dfs(root.Right, targetSum - root.Val)
 	}
-	dfs([]int{}, root, targetSum)
+	dfs(root, targetSum)
 
 	return
 }
 //leetcode submit region end(Prohibit modification and deletion)
+
+/**
+func pathSum(root *TreeNode, sum int) (ans [][]int) {
+    path := []int{}
+    var dfs func(*TreeNode, int)
+    dfs = func(node *TreeNode, left int) {
+        if node == nil {
+            return
+        }
+        left -= node.Val
+        path = append(path, node.Val)
+        defer func() { path = path[:len(path)-1] }()
+        if node.Left == nil && node.Right == nil && left == 0 {
+            ans = append(ans, append([]int(nil), path...))
+            return
+        }
+        dfs(node.Left, left)
+        dfs(node.Right, left)
+    }
+    dfs(root, sum)
+    return
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/path-sum-ii/solution/lu-jing-zong-he-ii-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
