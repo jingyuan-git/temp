04c02f54fb1aa883455c9caf6ce5d6845584a9b2
jingyuan-git <lujingyuan95@gmail.com>
2021-03-09 17:54:20 +0800

    142 linked list cycle II

---
+++ leetcode/editor/cn/[142]Linked List Cycle II.go
@@ -0,0 +1,129 @@
+//Given a linked list, return the node where the cycle begins. If there is no cy
+//cle, return null. 
+//
+// There is a cycle in a linked list if there is some node in the list that can 
+//be reached again by continuously following the next pointer. Internally, pos is 
+//used to denote the index of the node that tail's next pointer is connected to. N
+//ote that pos is not passed as a parameter. 
+//
+// Notice that you should not modify the linked list. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [3,2,0,-4], pos = 1
+//Output: tail connects to node index 1
+//Explanation: There is a cycle in the linked list, where tail connects to the s
+//econd node.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,2], pos = 0
+//Output: tail connects to node index 0
+//Explanation: There is a cycle in the linked list, where tail connects to the f
+//irst node.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: head = [1], pos = -1
+//Output: no cycle
+//Explanation: There is no cycle in the linked list.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of the nodes in the list is in the range [0, 104]. 
+// -105 <= Node.val <= 105 
+// pos is -1 or a valid index in the linked-list. 
+// 
+//
+// 
+// Follow up: Can you solve it using O(1) (i.e. constant) memory? 
+// Related Topics 链表 双指针 
+// 👍 896 👎 0
+package main
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+    slow, fast := head, head
+    for fast != nil {
+    	slow = slow.Next
+    	if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		if fast == slow {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+
+
+/**
+func detectCycle(head *ListNode) *ListNode {
+    slow, fast := head, head
+    for fast != nil {
+        slow = slow.Next
+        if fast.Next == nil {
+            return nil
+        }
+        fast = fast.Next.Next
+        if fast == slow {
+            p := head
+            for p != slow {
+                p = p.Next
+                slow = slow.Next
+            }
+            return p
+        }
+    }
+    return nil
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/huan-xing-lian-biao-ii-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+
+func detectCycle(head *ListNode) *ListNode {
+    seen := map[*ListNode]struct{}{}
+    for head != nil {
+        if _, ok := seen[head]; ok {
+            return head
+        }
+        seen[head] = struct{}{}
+        head = head.Next
+    }
+    return nil
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/huan-xing-lian-biao-ii-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
