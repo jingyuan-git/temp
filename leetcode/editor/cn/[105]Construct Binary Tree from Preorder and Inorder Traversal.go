e73191f3bd2968cc8e83684d3e6983b5906ed575
jingyuan <lujingyuan95@gmail.com>
2021-03-18 00:47:42 +0800

    Create [105]Construct Binary Tree from Preorder and Inorder Traversal.go

---
+++ leetcode/editor/cn/[105]Construct Binary Tree from Preorder and Inorder Traversal.go
@@ -0,0 +1,48 @@
+//Given two integer arrays preorder and inorder where preorder is the preorder t
+//raversal of a binary tree and inorder is the inorder traversal of the same tree,
+// construct and return the binary tree. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//Output: [3,9,20,null,null,15,7]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: preorder = [-1], inorder = [-1]
+//Output: [-1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= preorder.length <= 3000 
+// inorder.length == preorder.length 
+// -3000 <= preorder[i], inorder[i] <= 3000 
+// preorder and inorder consist of unique values. 
+// Each value of inorder also appears in preorder. 
+// preorder is guaranteed to be the preorder traversal of the tree. 
+// inorder is guaranteed to be the inorder traversal of the tree. 
+// 
+// Related Topics 树 深度优先搜索 数组 
+// 👍 936 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
