2eb88d0e07a82acaee113b92738388b7e946866e
jingyuan <lujingyuan95@gmail.com>
2021-03-22 13:08:06 +0800

    [145]Binary Tree Postorder Traversal

---
+++ leetcode/editor/cn/[145]Binary Tree Postorder Traversal.go
@@ -0,0 +1,84 @@
+//Given the root of a binary tree, return the postorder traversal of its nodes' 
+//values. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [1,null,2,3]
+//Output: [3,2,1]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = [1]
+//Output: [1]
+// 
+//
+// Example 4: 
+//
+// 
+//Input: root = [1,2]
+//Output: [2,1]
+// 
+//
+// Example 5: 
+//
+// 
+//Input: root = [1,null,2]
+//Output: [2,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of the nodes in the tree is in the range [0, 100]. 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// Follow up: 
+//
+// Recursive solution is trivial, could you do it iteratively? 
+//
+// 
+// Related Topics 栈 树 
+// 👍 545 👎 0
+
+package main
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func postorderTraversal(root *TreeNode) []int {
+	stack := []*TreeNode{}
+	var result []int
+
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		root = stack[len(stack)-1].Right
+		result = append(result, root.Val)
+		stack = stack[:len(stack)-1]
+	}
+	return result
+}
+//leetcode submit region end(Prohibit modification and deletion)
