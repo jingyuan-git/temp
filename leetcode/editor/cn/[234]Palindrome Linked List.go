696410602f302522bbc5947fa34c17a6cfe531d0
jingyuan <lujingyuan95@gmail.com>
2021-07-07 23:58:46 +0800

    start [234]Palindrome Linked List

---
+++ leetcode/editor/cn/[234]Palindrome Linked List.go
@@ -0,0 +1,43 @@
+//Given the head of a singly linked list, return true if it is a palindrome. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,2,2,1]
+//Output: true
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,2]
+//Output: false
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range [1, 105]. 
+// 0 <= Node.val <= 9 
+// 
+//
+// 
+//Follow up: Could you do it in O(n) time and O(1) space? Related Topics 栈 递归 链表
+// 双指针 
+// 👍 1039 👎 0
+package main
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
