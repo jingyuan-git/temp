4ecb276c974860db7f8a321ff41c30f42e2beba9
jingyuan <lujingyuan95@gmail.com>
2021-07-23 10:32:26 +0800

    Update [114]Flatten Binary Tree to Linked List.go

--- leetcode/editor/cn/[114]Flatten Binary Tree to Linked List.go
+++ leetcode/editor/cn/[114]Flatten Binary Tree to Linked List.go
@@ -53,6 +53,60 @@ package main
  *     Right *TreeNode
  * }
  */
+// 思路：把右节点移到左节点的右侧
+// https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/solution/er-cha-shu-zhan-kai-wei-lian-biao-by-leetcode-solu/
+func flatten(root *TreeNode)  {
+	curr := root
+	for curr != nil {
+		if curr.Left != nil {
+			pre := curr.Left
+			for pre.Right != nil {
+				pre = pre.Right
+			}
+			pre.Right = curr.Right
+			curr.Right = curr.Left
+			// 左边置空！！！
+			curr.Left = nil
+		}
+		curr = curr.Right
+	}
+}
+
+
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+/**
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null){
+            return ;
+        }
+        //将根节点的左子树变成链表
+        flatten(root.left);
+        //将根节点的右子树变成链表
+        flatten(root.right);
+        TreeNode temp = root.right;
+        //把树的右边换成左边的链表
+        root.right = root.left;
+        //记得要将左边置空
+        root.left = null;
+        //找到树的最右边的节点
+        while(root.right != null) root = root.right;
+        //把右边的链表接到刚才树的最右边的节点
+        root.right = temp;
+    }
+}
+
+作者：Geralt_U
+链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/solution/114-er-cha-shu-zhan-kai-wei-lian-biao-by-ming-zhi-/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+
+
+
+/**
 func flatten(root *TreeNode)  {
 	list := preorderTraversal(root)
 	for i := 1; i < len(list); i++ {
@@ -70,4 +124,4 @@ func preorderTraversal(root *TreeNode) []*TreeNode {
 	}
 	return list
 }
-//leetcode submit region end(Prohibit modification and deletion)
+ */
